1041c9356c97602908ea82cf1f1cbd75807aa838
alyssayuan17 <119757032+alyssayuan17@users.noreply.github.com>
2025-09-21 10:44:27 -0400

    remove build, install, and log directories

--- build/planner/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /ws/src/robot/planner
-# Build directory: /ws/build/planner
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
