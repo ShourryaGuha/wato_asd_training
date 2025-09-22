664709e83c36b78b0784f4842495e2460b27ef6b
shourrya <guhashourrya@gmail.com>
2025-09-21 23:34:05 -0400

    small stuuf

--- src/robot/map_memory/src/map_memory_node.cpp
+++ src/robot/map_memory/src/map_memory_node.cpp
@@ -14,6 +14,26 @@ MapMemoryNode::MapMemoryNode() : Node("map_memory"), map_memory_(robot::MapMemor
   // Initialize timer
   timer_ = this->create_wall_timer(
       std::chrono::seconds(1), std::bind(&MapMemoryNode::timerCallback, this));
+
+  global_map_ = nav_msgs::msg::OccupancyGrid();
+  latest_costmap_ = nav_msgs::msg::OccupancyGrid();
+
+  global_map_.header.stamp = this->now();
+  global_map_.header.frame_id = "map";
+  global_map_.info.resolution = RES;
+  global_map_.info.width = COST_MAP_WIDTH;
+  global_map_.info.height = COST_MAP_HEIGHT;
+  global_map_.info.origin.position.x = -COST_MAP_WIDTH * RES / 2.0;
+  global_map_.info.origin.position.y = -COST_MAP_HEIGHT * RES / 2.0;
+  global_map_.data.assign(COST_MAP_WIDTH * COST_MAP_HEIGHT, 0);
+}
+
+// Callback for costmap updates
+void MapMemoryNode::costmapCallback(const nav_msgs::msg::OccupancyGrid::SharedPtr msg)
+{
+  // Store the latest costmap
+  latest_costmap_ = *msg;
+  updated_costmap_ = true;
 }
 
 int main(int argc, char **argv)
